666043fb6b6e4b7743bffa0dafee02aa36d8c61d
Randy Reyes <rqreyes@gmail.com>
2025-02-02 16:07:30 +0800

    feat: add 2 player mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
-#include <raylib.h>
 #include <iostream>
+#include <raylib.h>
+
+typedef enum
+{
+	TITLE,
+	MODE_1_PLAYER,
+	MODE_2_PLAYER
+} screen_t;
 
 Color green = Color{38, 185, 154, 255};
 Color green_dark = Color{20, 160, 133, 255};
 Color green_light = Color{129, 204, 184, 255};
 Color yellow = Color{243, 213, 91, 255};
 
-int score_player = 0;
+int score_player1 = 0;
 int score_cpu = 0;
+int score_player2 = 0;
 
 class Ball
 {
@@ -31,12 +39,13 @@ public:
 		}
 		if (x + radius >= GetScreenWidth())
 		{
-			score_cpu += 1;
+			score_player1 += 1;
 			ResetBall();
 		}
 		if (x - radius <= 0)
 		{
-			score_player += 1;
+			score_player2 += 1;
+			score_cpu += 1;
 			ResetBall();
 		}
 	}
@@ -75,13 +84,33 @@ public:
 	{
 		DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
 	}
+};
+class PaddlePlayer1 : public Paddle
+{
+public:
 	void Update()
 	{
-		if (IsKeyDown(KEY_UP))
+		if (IsKeyDown(KEY_W))
 		{
 			y -= speed;
 		}
-		if (IsKeyDown(KEY_DOWN))
+		if (IsKeyDown(KEY_S))
+		{
+			y += speed;
+		}
+		LimitMovement();
+	}
+};
+class PaddlePlayer2 : public Paddle
+{
+public:
+	void Update()
+	{
+		if (IsKeyDown(KEY_I))
+		{
+			y -= speed;
+		}
+		if (IsKeyDown(KEY_K))
 		{
 			y += speed;
 		}
@@ -106,7 +135,8 @@ public:
 };
 
 Ball ball;
-Paddle player;
+PaddlePlayer1 player1;
+PaddlePlayer2 player2;
 PaddleCpu cpu;
 
 // --------------------
@@ -122,9 +152,10 @@ int main()
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "my pong game");
 	SetTargetFPS(60);
 
-	bool startGame = false;
+	screen_t screen_current = TITLE;
+
 	int font_size = 80;
-	const char *text = "Press spacebar to start";
+	const char *text = "My Pong Game";
 	const int text_width_x = MeasureText(text, font_size);
 
 	ball.radius = 20;
@@ -133,15 +164,21 @@ int main()
 	ball.speed_x = 7;
 	ball.speed_y = 7;
 
-	player.width = 25;
-	player.height = 120;
-	player.x = SCREEN_WIDTH - player.width - 10;
-	player.y = SCREEN_HEIGHT / 2 - player.height / 2;
-	player.speed = 6;
+	player1.width = 25;
+	player1.height = 120;
+	player1.x = 10;
+	player1.y = SCREEN_HEIGHT / 2 - player1.height / 2;
+	player1.speed = 6;
+
+	player2.width = 25;
+	player2.height = 120;
+	player2.x = SCREEN_WIDTH - player2.width - 10;
+	player2.y = SCREEN_HEIGHT / 2 - player2.height / 2;
+	player2.speed = 6;
 
 	cpu.width = 25;
 	cpu.height = 120;
-	cpu.x = 10;
+	cpu.x = SCREEN_WIDTH - cpu.width - 10;
 	cpu.y = SCREEN_HEIGHT / 2 - cpu.height / 2;
 	cpu.speed = 6;
 
@@ -149,48 +186,87 @@ int main()
 	// --------------------
 	while (!WindowShouldClose())
 	{
+		// update objects
+		// --------------------
+		switch (screen_current)
+		{
+		case TITLE:
+			if (IsKeyPressed(KEY_ONE))
+			{
+				screen_current = MODE_1_PLAYER;
+			}
+			else if (IsKeyPressed(KEY_TWO))
+			{
+				screen_current = MODE_2_PLAYER;
+			}
+
+			break;
+		case MODE_1_PLAYER:
+			ball.Update();
+			player1.Update();
+			cpu.Update(ball.y);
+
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+			{
+				ball.speed_x *= -1;
+			}
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.height}))
+			{
+				ball.speed_x *= -1;
+			}
+
+			break;
+		case MODE_2_PLAYER:
+			ball.Update();
+			player1.Update();
+			player2.Update();
+
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.height}))
+			{
+				ball.speed_x *= -1;
+			}
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.height}))
+			{
+				ball.speed_x *= -1;
+			}
+
+			break;
+		default:
+			break;
+		}
+
+		// draw objects
+		// --------------------
 		BeginDrawing();
 		ClearBackground(green_dark);
 		DrawRectangle(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, green);
 		DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 150, green_light);
 		DrawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, WHITE);
 
-		// game screen
-		if (startGame)
+		switch (screen_current)
 		{
-			// update positions
-			// --------------------
-			ball.Update();
-			player.Update();
-			cpu.Update(ball.y);
-
-			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
-			{
-				ball.speed_x *= -1;
-			}
-			else if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
-			{
-				ball.speed_x *= -1;
-			}
-
-			// draw objects
-			// --------------------
-			ball.Draw();
-			cpu.Draw();
-			player.Draw();
-			DrawText(TextFormat("%i", score_cpu), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_cpu), font_size) / 2), 20, font_size, WHITE);
-			DrawText(TextFormat("%i", score_player), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_player), font_size) / 2), 20, font_size, WHITE);
-		}
-		// start screen
-		else
-		{
-
+		case TITLE:
 			DrawText(text, SCREEN_WIDTH / 2 - text_width_x / 2, SCREEN_HEIGHT / 2 - font_size / 2, font_size, WHITE);
 
-			if (IsKeyPressed(KEY_SPACE))
-			{
-				startGame = true;
-			}
+			break;
+		case MODE_1_PLAYER:
+			ball.Draw();
+			player1.Draw();
+			cpu.Draw();
+			DrawText(TextFormat("%i", score_player1), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_player1), font_size) / 2), 20, font_size, WHITE);
+			DrawText(TextFormat("%i", score_cpu), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_cpu), font_size) / 2), 20, font_size, WHITE);
+
+			break;
+		case MODE_2_PLAYER:
+			ball.Draw();
+			player1.Draw();
+			player2.Draw();
+			DrawText(TextFormat("%i", score_player1), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_player1), font_size) / 2), 20, font_size, WHITE);
+			DrawText(TextFormat("%i", score_player2), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_player2), font_size) / 2), 20, font_size, WHITE);
+
+			break;
+		default:
+			break;
 		}
 
 		EndDrawing();
