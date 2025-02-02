dde2ece78bce625bafe388c4730415d12dc3ebec
Randy Reyes <rqreyes@gmail.com>
2025-02-02 16:50:21 +0800

    feat: add content in title screen

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,18 @@ typedef enum
 	MODE_2_PLAYER
 } screen_t;
 
+const int font_size_h1 = 80;
+const int font_size_h2 = 40;
+const int font_size_p = 20;
+
 Color green = Color{38, 185, 154, 255};
 Color green_dark = Color{20, 160, 133, 255};
 Color green_light = Color{129, 204, 184, 255};
 Color yellow = Color{243, 213, 91, 255};
 
-int score_player1 = 0;
+int score_player_1 = 0;
 int score_cpu = 0;
-int score_player2 = 0;
+int score_player_2 = 0;
 
 class Ball
 {
@@ -39,12 +43,12 @@ public:
 		}
 		if (x + radius >= GetScreenWidth())
 		{
-			score_player1 += 1;
+			score_player_1 += 1;
 			ResetBall();
 		}
 		if (x - radius <= 0)
 		{
-			score_player2 += 1;
+			score_player_2 += 1;
 			score_cpu += 1;
 			ResetBall();
 		}
@@ -135,8 +139,8 @@ public:
 };
 
 Ball ball;
-PaddlePlayer1 player1;
-PaddlePlayer2 player2;
+PaddlePlayer1 player_1;
+PaddlePlayer2 player_2;
 PaddleCpu cpu;
 
 // --------------------
@@ -149,14 +153,10 @@ int main()
 	const int SCREEN_WIDTH = 1280;
 	const int SCREEN_HEIGHT = 800;
 
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "my pong game");
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My Pong Game");
 	SetTargetFPS(60);
 
 	screen_t screen_current = TITLE;
-
-	int font_size = 80;
-	const char *text = "My Pong Game";
-	const int text_width_x = MeasureText(text, font_size);
 
 	ball.radius = 20;
 	ball.x = SCREEN_WIDTH / 2;
@@ -164,17 +164,17 @@ int main()
 	ball.speed_x = 7;
 	ball.speed_y = 7;
 
-	player1.width = 25;
-	player1.height = 120;
-	player1.x = 10;
-	player1.y = SCREEN_HEIGHT / 2 - player1.height / 2;
-	player1.speed = 6;
+	player_1.width = 25;
+	player_1.height = 120;
+	player_1.x = 10;
+	player_1.y = SCREEN_HEIGHT / 2 - player_1.height / 2;
+	player_1.speed = 6;
 
-	player2.width = 25;
-	player2.height = 120;
-	player2.x = SCREEN_WIDTH - player2.width - 10;
-	player2.y = SCREEN_HEIGHT / 2 - player2.height / 2;
-	player2.speed = 6;
+	player_2.width = 25;
+	player_2.height = 120;
+	player_2.x = SCREEN_WIDTH - player_2.width - 10;
+	player_2.y = SCREEN_HEIGHT / 2 - player_2.height / 2;
+	player_2.speed = 6;
 
 	cpu.width = 25;
 	cpu.height = 120;
@@ -203,14 +203,14 @@ int main()
 			break;
 		case MODE_1_PLAYER:
 			ball.Update();
-			player1.Update();
+			player_1.Update();
 			cpu.Update(ball.y);
 
 			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
 			{
 				ball.speed_x *= -1;
 			}
-			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.height}))
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player_1.x, player_1.y, player_1.width, player_1.height}))
 			{
 				ball.speed_x *= -1;
 			}
@@ -218,14 +218,14 @@ int main()
 			break;
 		case MODE_2_PLAYER:
 			ball.Update();
-			player1.Update();
-			player2.Update();
+			player_1.Update();
+			player_2.Update();
 
-			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.height}))
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player_1.x, player_1.y, player_1.width, player_1.height}))
 			{
 				ball.speed_x *= -1;
 			}
-			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.height}))
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player_2.x, player_2.y, player_2.width, player_2.height}))
 			{
 				ball.speed_x *= -1;
 			}
@@ -246,23 +246,34 @@ int main()
 		switch (screen_current)
 		{
 		case TITLE:
-			DrawText(text, SCREEN_WIDTH / 2 - text_width_x / 2, SCREEN_HEIGHT / 2 - font_size / 2, font_size, WHITE);
+		{
+			const char *title_text = "My Pong Game";
+			const int title_width = MeasureText(title_text, font_size_h1);
+			const char *subtitle_text_1 = "Press 1 for 1-player mode";
+			const int subtitle_width_1 = MeasureText(subtitle_text_1, font_size_h2);
+			const char *subtitle_text_2 = "Press 2 for 2-player mode";
+			const int subtitle_width_2 = MeasureText(subtitle_text_2, font_size_h2);
+
+			DrawText(title_text, SCREEN_WIDTH / 2 - title_width / 2, 300, font_size_h1, WHITE);
+			DrawText(subtitle_text_1, SCREEN_WIDTH / 2 - subtitle_width_1 / 2, 400, font_size_h2, WHITE);
+			DrawText(subtitle_text_2, SCREEN_WIDTH / 2 - subtitle_width_2 / 2, 450, font_size_h2, WHITE);
 
 			break;
+		}
 		case MODE_1_PLAYER:
 			ball.Draw();
-			player1.Draw();
+			player_1.Draw();
 			cpu.Draw();
-			DrawText(TextFormat("%i", score_player1), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_player1), font_size) / 2), 20, font_size, WHITE);
-			DrawText(TextFormat("%i", score_cpu), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_cpu), font_size) / 2), 20, font_size, WHITE);
+			DrawText(TextFormat("%i", score_player_1), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_player_1), font_size_h1) / 2), 20, font_size_h1, WHITE);
+			DrawText(TextFormat("%i", score_cpu), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_cpu), font_size_h1) / 2), 20, font_size_h1, WHITE);
 
 			break;
 		case MODE_2_PLAYER:
 			ball.Draw();
-			player1.Draw();
-			player2.Draw();
-			DrawText(TextFormat("%i", score_player1), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_player1), font_size) / 2), 20, font_size, WHITE);
-			DrawText(TextFormat("%i", score_player2), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_player2), font_size) / 2), 20, font_size, WHITE);
+			player_1.Draw();
+			player_2.Draw();
+			DrawText(TextFormat("%i", score_player_1), (SCREEN_WIDTH / 4) - (MeasureText(TextFormat("%i", score_player_1), font_size_h1) / 2), 20, font_size_h1, WHITE);
+			DrawText(TextFormat("%i", score_player_2), (SCREEN_WIDTH * 3 / 4) - (MeasureText(TextFormat("%i", score_player_2), font_size_h1) / 2), 20, font_size_h1, WHITE);
 
 			break;
 		default:
